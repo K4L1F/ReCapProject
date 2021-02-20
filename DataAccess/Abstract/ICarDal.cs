c2f36706cdfecf791b722b8232d3bde9b428916f
Taha vefa Sözer <tvefa06@gmail.com>
2021-02-20 18:03:14 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Car> GetAll();
+        List<Car> GetById(int Id);
+        void Add(ICarDal car);
+        void Update(ICarDal car);
+        void Delete(ICarDal car);
+    }
+}
---
+++ ICarService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICarService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
